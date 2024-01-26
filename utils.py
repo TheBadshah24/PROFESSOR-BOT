603bff9aa427901c679174d21406c71e8bb5745a
TheBlackXYZ24 <155623566+TheBlackxyz@users.noreply.github.com>
2024-01-26 12:56:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -304,8 +304,8 @@ def get_time(seconds):
     return result
     
 async def get_shortlink(link):
-    url = f'{SHORT_URL}/api'
-    params = {'api': SHORT_API, 'url': link}
+    url = f'https://shareus.io/api'
+    params = {'QkPZEY0lolfg6KOfgk3019OaHHk2': SHORT_API, 'url': link}
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
